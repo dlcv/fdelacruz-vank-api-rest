862b16b883a2ba59b8cbf2b8f461fda82e5db5ae
fdelacruz-at-092623405260 <dlcv@live.com.ve>
2021-12-16 23:14:08 -0400

    Client features: getAllClients, getOneClient, updateClient and saveClient

--- server/controllers/clients.controller.js
+++ server/controllers/clients.controller.js
@@ -1,3 +1,4 @@
+const { Client } = require('../config/database');
 const { logger } = require('../config/logs');
 const validate = require('../middlewares/validations');
 
@@ -35,32 +36,85 @@ clientController.saveClient = async(req, res, next) => {
         });
 
     } else {
-        return res.json({
-            company_name,
-            internal_code,
-            tax_id,
-            currency,
-            api_quota,
-            bank_id
-        })
+        try {
+            let client = await Client.create(req.body);
+            let message = "Client created successfully";
+            logger.info(`201 || ${ message } - ${ req.originalUrl } - ${ req.method } - ${ req.ip }`);
+            return res.status(201).json({
+                message,
+            });
+
+        } catch (error) {
+            logger.error(`500 || ${ error.message } - ${ req.originalUrl } - ${ req.method } - ${ req.ip }`);
+            return res.status(500).json({
+                message: error.message
+            });
+        }
+    }
+};
+
+// Update client
+clientController.updateClient = async(req, res, next) => {
+    // Errors
+    let errors = [];
+
+    // Data from request
+    const { tax_id, currency } = req.body;
+
+    // Validations
+    if (typeof req.body === 'undefined') {
+        errors.push({ error: "request body can't be empty" });
+    } else {
+        // Check validations
+        errors = errors.concat(validate.validateTaxId(tax_id))
+        errors = errors.concat(validate.validateCurrency(currency))
     }
 
-    // Send response
-    /*
-    try {
-        let message = "¡API is working fine!";
-        logger.info(`200 || ${ message } - ${ req.originalUrl } - ${ req.method } - ${ req.ip }`);
-        return res.json({
-            message: "success",
+    if (errors.length > 0) {
+        // Return errors
+        for (let index = 0; index < errors.length; index++) {
+            logger.error(`400 || ${ errors[index].error } - ${ req.originalUrl } - ${ req.method } - ${ req.ip }`);
+        }
+        return res.status(400).json({
+            message: errors
         });
 
-    } catch (error) {
-        logger.error(`500 || ${ error.message } - ${ req.originalUrl } - ${ req.method } - ${ req.ip }`);
-        return res.status(500).json({
-            message: error.message
-        });
+    } else {
+        try {
+            await Client.update(req.body, {
+                where: {
+                    id: req.params.id
+                }
+            });
+            let message = "Client updated successfully";
+            logger.info(`200 || ${ message } - ${ req.originalUrl } - ${ req.method } - ${ req.ip }`);
+            return res.status(200).json({
+                message,
+            });
+
+        } catch (error) {
+            logger.error(`500 || ${ error.message } - ${ req.originalUrl } - ${ req.method } - ${ req.ip }`);
+            return res.status(500).json({
+                message: error.message
+            });
+        }
     }
-    */
+};
+
+// Get all clients
+clientController.getAllClients = async(req, res, next) => {
+    const clients = await Client.findAll();
+    return res.json(clients);
+};
+
+// Get one clients
+clientController.getOneClient = async(req, res, next) => {
+    const client = await Client.findAll({
+        where: {
+            id: req.params.id
+        }
+    });
+    return res.json(client);
 };
 
 module.exports = clientController;
--- server/routes/clients.routes.js
+++ server/routes/clients.routes.js
@@ -2,11 +2,14 @@ const { Router } = require('express');
 const { logger } = require('../config/logs');
 // const { verifyToken, verifyAPIKey } = require('../middlewares/auth');
 
-const { saveClient } = require('../controllers/clients.controller');
+const { saveClient, getAllClients, getOneClient, updateClient } = require('../controllers/clients.controller');
 
 const router = Router();
 
 // Client routes
-router.post('/client/', saveClient);
+router.post('/api/v1/client/', saveClient);
+router.get('/api/v1/client/', getAllClients);
+router.get('/api/v1/client/:id', getOneClient);
+router.put('/api/v1/client/:id', updateClient);
 
 module.exports = router;
