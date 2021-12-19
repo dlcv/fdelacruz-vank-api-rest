84bc42dbeb91177ee5f5969bd117ef4426cda534
fdelacruz-at-092623405260 <dlcv@live.com.ve>
2021-12-19 08:31:46 -0400

    Optimizing env variables

--- server/config/database.js
+++ server/config/database.js
@@ -5,29 +5,11 @@ const BankModel = require("../models/bank.model");
 const ClientModel = require("../models/client.model");
 const InvoiceModel = require("../models/invoice.model");
 
-// DATABASE HOST
-const DB_HOST = process.env.DB_HOST;
-
-// DATABASE USER
-const DB_USER = process.env.DB_USER;
-
-// DATABASE PASS
-const DB_PASS = process.env.DB_PASS;
-
-// DATABASE PORT
-const DB_PORT = process.env.DB_PORT;
-
-// DATABASE NAME
-const DB_NAME = process.env.DB_NAME;
-
-// DATABASE DIALECT
-const DB_DIALECT = process.env.DB_DIALECT;
-
-const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASS, {
-    host: DB_HOST,
-    port: DB_PORT,
-    dialect: DB_DIALECT,
-    logging: false
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: process.env.DB_DIALECT,
+    logging: false // process.env.DB_LOGGING
 })
 
 const Bank = BankModel(sequelize, Sequelize);
--- server/config/scheduler.js
+++ server/config/scheduler.js
@@ -1,34 +1,27 @@
+require("dotenv").config();
+
 const fs = require("fs");
 const https = require("https");
-
 const schedule = require("node-schedule");
-
-const invoiceController = require("../controllers/invoices.controller")
+const invoiceController = require("../controllers/invoices.controller");
 
 const rule = new schedule.RecurrenceRule();
 // rule.hour = 0;
 rule.second = 0;
 
-
-// Directory for CSV"s files
-var dir = "./csv";
-
 // Create directory if not exist
-if (!fs.existsSync(dir)) {
-    fs.mkdirSync(dir);
+if (!fs.existsSync(process.env.APP_CSV_DIR_PATH)) {
+    fs.mkdirSync(process.env.APP_CSV_DIR_PATH);
 }
 
 // Create empty CSV file
-const file = fs.createWriteStream("./csv/invoices.csv");
-
-// External URL for CSV file
-const url = "https://gist.githubusercontent.com/rogelio-meza-t/f70a484ec20b8ea43c67f95a58597c29/raw/41f289c605718e923fc1fad0539530e4d0413a90/invoices.csv";
+const file = fs.createWriteStream(process.env.APP_CSV_DIR_PATH + "/" + process.env.APP_CSV_FILE_NAME);
 
 // Job
 const Job = schedule.scheduleJob(rule, function() {
     console.log("Init schedule job");
     // Get external file
-    https.get(url, response => {
+    https.get(process.env.APP_EXTERNAL_CSV_URL, response => {
         console.log("Getting external CSV file");
         // Write local file
         var stream = response.pipe(file);
